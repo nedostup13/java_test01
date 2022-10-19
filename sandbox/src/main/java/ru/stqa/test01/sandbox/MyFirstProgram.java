023a9baae493dcd8221cb0974271509b1f709bbf
nedostup13 <nedostup13@users.noreply.github.com>
2022-10-19 19:51:49 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/test01/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/test01/sandbox/MyFirstProgram.java
@@ -3,7 +3,40 @@ package ru.stqa.test01.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+
+    hello("world");
+    hello("user");
+    hello("Alexei");
+    hello("all");
+
+    double radius = 3;
+    System.out.println("Площадь круга с радиусом " + radius + " равна " + areaCircle(radius));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Площадь прямоугольника со стороной " + a + " и стороной " + b + " равна " + areaRectangle(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double areaCircle(double radius) {
+
+    double S;
+    S = Math.PI * square(radius);
+    return S;
+  }
+
+  public static double square(double a) {
+    return a * a;
+  }
+
+  public static double areaRectangle(double a, double b) {
+    return a * b;
   }
 
 }
