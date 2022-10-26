7a36c4a1d254fe884f2848856f147b43bbcd70ef
nedostup13 <nedostup13@users.noreply.github.com>
2022-10-27 00:55:43 +0500

    обновлен метод инициализации фикстуры теста по созданию группы
    код оптимизирован логически:
    - в метод инициализации фикстуры перенесен код по авторизации в приложении АК;
    - в основном тесте оставлены только существенные действия по созданию группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationsTests.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class GroupCreationsTests {
@@ -17,10 +16,6 @@ public class GroupCreationsTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testGroupCreations() throws Exception {
     wd.get("http://localhost/addressbook/index.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -28,6 +23,10 @@ public class GroupCreationsTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreations() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("new")).click();
@@ -53,7 +52,6 @@ public class GroupCreationsTests {
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
-
   }
 
   private boolean isElementPresent(By by) {
