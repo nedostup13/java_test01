cf88ae38e249176f34ef1f31cdaf568fc59eb9d8
nedostup13 <nedostup13@users.noreply.github.com>
2022-11-25 01:28:09 +0300

    вычисление макс.идентификатора реализована через лямбда
    (через лямбда-выражения (анонимные функции))

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationsTests.java
@@ -17,24 +17,27 @@ public class GroupCreationsTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList(); // теперь в переменной before будет
     // содержаться список элементов, а не счетчик количества элементов
 
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
 
 //    int after = app.getGroupHelper().getGroupCount();
     List<GroupData> after = app.getGroupHelper().getGroupList(); // теперь в переменной after будет тоже список, а не счетчик
     Assert.assertEquals(after.size(), before.size() + 1);
 
+//    СТАРЫЙ СПОСОБ СРАВНЕНИЯ:
     //т.к. неизвестен новый идентификатор группы, мы понимаем, что у новой группы должен быть максимальный из всех возможных, входящих в after - т.е. это предсказанный ожидаемый идентификатор
     //при этом наш идентификатор был типа строка, а сравнивать необходимо их как числа
-    int max = 0;
+//    int max = 0;
     //цикл для проверки всех элементов на максимальный:
-    for (GroupData g : after) { //g - переменная, которая "пробегает" список after
-      if (g.getId() > max) { //если идент-р группы больше максимального,
-        max = g.getId();     //то присвоим максимуму знаение найденного идент-ра этой группы
-      }
-    }
-    group.setId(max); //присваиваем самый большой идент-р присваевается как идентификатор новой группы
+//    for (GroupData g : after) { //g - переменная, которая "пробегает" список after
+//      if (g.getId() > max) { //если идент-р группы больше максимального,
+//        max = g.getId();     //то присвоим максимуму знаение найденного идент-ра этой группы
+//      }
+//    }
 
+    //    НОВЫЙ СПОСОБ СРАВНЕНИЯ (только в Java 8) через компаратор:
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId()); //присваиваем самый большой идент-р присваевается как идентификатор новой группы
     before.add(group); //добавлем в старый список ту группу, которую создали самостоятельно(модифицировали); здесь мы предсказываемый ожидаемый результат
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); //сравниваемый старый список с новым без учета порядка
     //оба списка преобразовываем в множества и сравниваем
